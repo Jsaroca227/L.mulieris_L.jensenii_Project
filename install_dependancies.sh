8fa666963d4c8f922522a1abcef431e4885a5abc
Max Bates <mbates5@luc.edu>
2025-04-09 18:57:59 -0500

    Organizing github repo

--- install_dependancies.sh
+++
@@ -1,6 +0,0 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-source ~/miniconda3/bin/activate
-conda --version
-conda update conda
-conda install -c conda-forge -c bioconda sylph
