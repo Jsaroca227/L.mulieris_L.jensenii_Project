fd32fee3116d12beee2a04be5ea9fad2b5259377
Max Bates <mbates5@luc.edu>
2025-04-10 17:27:04 -0500

    Add confirmed positive SRAs, edit comments on script

--- sylph_wrapper.py
+++ sylph_wrapper.py
@@ -1,10 +1,8 @@
-##this code should contain the running of sylph
-
 import os
 import sys
 import argparse
 
-#function to parse command line arguments
+# function to parse command line arguments
 def check_arg(args=None):
     parser = argparse.ArgumentParser(
     description="ADD TITLE OF SCRIPT HERE (shows on help -h)")
@@ -23,7 +21,7 @@ def check_arg(args=None):
 
     return parser.parse_args(args)
 
-#retrieve command line arguments
+# retrieve command line arguments
 arguments = check_arg(sys.argv[1:])
 SRA_LIST_FILE = arguments.sra_file
 SYLPH_DB_PATH = arguments.path_db
@@ -31,7 +29,7 @@ THREADS = arguments.threads
 OUTPUT_DIR = "sra_downloads"
 LOG_FILE = "processed_sra.log"
 
-#loads the processed SRA in a log file
+# loads the processed SRAs
 processed_sra = set()
 if os.path.exists(LOG_FILE):
     with open(LOG_FILE, "r") as log:
@@ -50,64 +48,65 @@ for sra in sra_list:
 sra_list = sra_list_temp
 
 flag = False
-
+# Looping through SRAs
 while True:
     for sra_id_temp in sra_list:
         processing = []
+        # binning SRAs that have already been processed
         if os.path.exists("processing.log"):
             with open("processing.log","r") as file:
                 processing = file.read()
                 processing = processing.splitlines()
         if (sra_id_temp in processed_sra) or (sra_id_temp in processing):
-            continue #skips the SRA IDs that have already been processed
+            continue # skips the SRA IDs that have already been processed
         else:
             sra_id = sra_id_temp
         if set(sra_list) == processed_sra:
             flag = True
-
+    # if all SRAs have been processed break the loop
     if flag:
         break
 
     print(f"Processing {sra_id}...", flush = True)
+    # begin processing SRA, write SRA to log files
     with open("results.tsv","a") as file:
         file.write(sra_id + "\n")
     with open("processing.log",'a') as file:
         file.write(f"{sra_id}\n")
 
-    #creates the directory inside of the loop in order to store all things related to the SRA there
+    # creates the directory inside of the loop in order to store all things related to the SRA there
     sra_dir = os.path.join(OUTPUT_DIR, sra_id)
     os.makedirs(sra_dir, exist_ok=True)
 
-    #downloads the SRA
+    # downloads the SRA
     os.system(f"prefetch {sra_id} -O {sra_dir}")
 
+    # run fasterq-dump
     print(f"Running fasterq-dump on {sra_id}!", flush = True)
-    #converts it to a fastqfile
     os.system(f"fasterq-dump --threads {THREADS} --split-files --outdir {sra_dir} {os.path.join(sra_dir, sra_id)}")
     
-    #defines the path of the fastq file
+    # define path of the fastq file
     fastq_1 = f"{sra_dir}/{sra_id}_1.fastq"
     fastq_2 = f"{sra_dir}/{sra_id}_2.fastq"
 
-    #statments for debugging purposes
+    # statments for debugging purposes, now supports sralite files
     if os.path.exists(fastq_1) and os.path.exists(fastq_2):
         print(f"FASTQ files generated successfully for {sra_id}!", flush = True)
     else:
         print(f"Error: FASTQ files missing for {sra_id}, attempting .sralite!", flush = True)
         os.system(f"fasterq-dump --threads {THREADS} --split-files --outdir {sra_dir} {os.path.join(sra_dir, sra_id, sra_id + '.sralite')}")
-
     if os.path.exists(fastq_1) and os.path.exists(fastq_2):
         print(f"FASTQ files generated successfully for {sra_id}!", flush = True)
     else:
         print(f"Error: FASTQ files missing for {sra_id}!", flush = True)
         break
 
-    #processes ANI with sylph
+    # process SRA with sylph
     os.system(f"sylph sketch -t {THREADS} -c 45 -1 {fastq_1} -2 {fastq_2} -d {sra_dir}")
     os.system(f"sylph profile -u --read-seq-id 99 -t {THREADS} --min-number-kmers 2 >> results.tsv {SYLPH_DB_PATH} {fastq_1}.paired.sylsp")
 
 
-    #log the successful processing of sylph/ANI
+    # log sylph results
     with open(LOG_FILE, "a") as log:
         log.write(sra_id + "\n")
         processed_sra.add(sra_id)
@@ -119,12 +118,12 @@ while True:
             if line.strip("\n") != sra_id:
                 f.write(line)
 
-    #remove SRA for free data purposes
+    # remove SRA for free data purposes
     os.system(f"rm -r {sra_dir}")
 
     print(f"Finished processing {sra_id} and removed directory.", flush = True)
 
-    #checks if all 482 SRA has been processed, if not contine the loop
+    # checks if all SRAs have been processed
     if len(processed_sra) >= SRA_LIST_LENGTH:
         print(f"All {SRA_LIST_LENGTH} SRA IDs have been processed.", flush = True)
         break
